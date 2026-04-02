48a89b736efb7cd6173a220a718dd7d1906cdc3c
Romanov Dmitriy <lol.romanov@bk.ru>
2026-04-02 14:13:51 +0300

    Добавление логгера в код. Внедрение логгера в команды SearchCommand и GetArticleCommand через конструктор. Добавление обработки ошибок.

---
+++ dartpedia/cli/lib/src/logger.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+import 'package:logging/logging.dart';
+
+Logger initFileLogger(String name) {
+  hierarchicalLoggingEnabled = true;
+  final logger = Logger(name);
+  final now = DateTime.now();
+  final scriptFile = File(Platform.script.toFilePath());
+  final projectDir = scriptFile.parent.parent.path;
+  final dir = Directory('$projectDir/logs');
+  if (!dir.existsSync()) dir.createSync();
+  final logFile = File('${dir.path}/${now.year}_${now.month}_${now.day}_$name.txt');
+  
+  logFile.writeAsStringSync('=== Log started at ${DateTime.now()} ===\n', mode: FileMode.write);
+  print('Лог-файл создан: ${logFile.path}');
+  
+  logger.level = Level.ALL;
+  logger.onRecord.listen((record) {
+    final msg = '[${record.time} - ${record.loggerName}] ${record.level.name}: ${record.message}';
+    logFile.writeAsStringSync('$msg \n', mode: FileMode.append);
+  });
+  return logger;
+}
